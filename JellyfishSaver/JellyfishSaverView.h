24fc46b979b23c25b73c477c99bb0d0ceab77048
Eskil Gjerde Sviggum <eskilsviggum@gmail.com>
2023-12-01 12:15:37 +0100

    Initial Commit

---
+++ JellyfishSaver/JellyfishSaverView.h
@@ -0,0 +1,12 @@
+//
+//  JellyfishSaverView.h
+//  JellyfishSaver
+//
+//  Created by Eskil Gjerde Sviggum on 01/12/2023.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface JellyfishSaverView : ScreenSaverView
+
+@end
